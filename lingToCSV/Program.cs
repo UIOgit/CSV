11e40d554da749a8e0e166936b97feda28f8875f
Ivan <ivan7keys@mail.ru>
2023-10-08 19:54:28 +0300

    init

---
+++ lingToCSV/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQtoCSV;
+
+namespace linqToCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection<ToCSV> objectCollection = new Collection<ToCSV>();
+        }
+    }
+
+    public class ToCSV
+    {
+        public string Name;
+        public Guid ObjectID;
+        public string Name2;
+        public bool IsRight;
+        public double Width;
+        public double Height;
+
+        public ToCSV(string name, double w, double h)
+        {
+            Name = name;
+            ObjectID = Guid.NewGuid();
+            Name2 = string.Empty;
+            IsRight = false;
+            Width = w;
+            Height = h;
+        }
+    }
+
+}
